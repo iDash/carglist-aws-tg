4f94acc580d65553eadccd956c44209d9d397c5a
iDash <empty>
2024-04-12 15:27:33 -0700

    Add craglist, carfax, epicvin calls

---
+++ carfax.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from car import Carfax
+
+carfax_url = "https://pos.secure.carfax.com/vehicleDataByVin"
+headers = {"content-type": "application/json"}
+
+
+def carfax_price_range(vin):
+    print(f"[{vin}] Pull Carfax info")
+    data = {"vin": vin}
+    json_response = send_post_request(carfax_url, headers, json.dumps(data))
+    return Carfax(json_response) if json_response else None
+
+
+def send_post_request(url, headers, data):
+    try:
+        response = requests.post(url, headers=headers, data=data)
+        response.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx status codes)
+        return response.json()  # Assuming the response is JSON data
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending POST request: {e}")
+        return None
+
+
+# Example usage:
+# vin_number = "JTMZFREV8JJ160590"
+# carfax_data = carfax_price_range(vin_number)
+# print(vars(carfax_data))
---
+++ epicvin.py
@@ -0,0 +1,28 @@
+import requests
+from car import History
+
+api_key = "7d9d2a81-c8bd-4e72-ad32-1de6fa499017"
+url = "https://api.epicvin.com/check"
+
+
+def epicvin_info(vin):
+    print(f"[{vin}] Pull EpicVin info")
+    params = {"key": api_key, "vin": vin}
+    json_response = send_get_request(url, params)
+    return History(json_response.get("data")) if json_response else None
+
+
+def send_get_request(url, params=None):
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx status codes)
+        return response.json()  # Assuming the response is JSON data
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending GET request: {e}")
+        return None
+
+
+# Example usage:
+# vin_number = "2T3WFREV1JW482539"
+# epicvin_data = epicvin_info(vin_number)
+# print(vars(epicvin_data))
